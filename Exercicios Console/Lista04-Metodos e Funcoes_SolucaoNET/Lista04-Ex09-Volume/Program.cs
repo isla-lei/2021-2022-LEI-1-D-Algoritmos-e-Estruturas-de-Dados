e6ab56d2876f563c11e1933a0193b0a9b2ceb3f2
jjmoreira-projects <59443855+jjmoreira-projects@users.noreply.github.com>
2022-06-05 00:22:46 +0100

    Add files via upload

---
+++ Exercicios Console/Lista04-Metodos e Funcoes_SolucaoNET/Lista04-Ex09-Volume/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 9. Escreva uma função que calcule e retorne o volume de uma circunferência,
+// recebendo como parâmetro o raio.
+// Volume = 4/3.PI.r^3
+
+double Volume(double raio)
+{
+    double vol = (4 / 3) * Math.PI * Math.Pow(raio, 3);
+    return vol;
+}
+
+// Main
+// Pedir o raio ao utilizador
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.Write("Raio: ");
+double raio = double.Parse(Console.ReadLine());
+
+double resultado = Volume(raio);
+Console.WriteLine($"Volume: {resultado}");
+
---
+++ Exercicios Console/Lista04-Metodos e Funcoes_SolucaoNET/Lista04-Ex10-Formata Data/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// 10.Escreva um procedimento que imprima a
+// data recebida em 3 parâmetros inteiros DD, MM e AAAA
+// na forma “DD de Nome do mês de AAAA”.
+// Exemplo: FormataData(29, 3, 2022) - Escreve: 29 de março de 2022
+
+string MesExtenso(int mesNumero)
+{
+    string mes = "";
+    switch (mesNumero)
+    {
+        case 1:
+            mes = "janeiro";
+            break;
+        case 2:
+            mes = "fevereiro";
+            break;
+        case 3:
+            mes = "março";
+            break;
+        case 4:
+            mes = "abril";
+            break;
+        case 5:
+            mes = "maio";
+            break;
+        case 6:
+            mes = "junho";
+            break;
+        case 7:
+            mes = "julho";
+            break;
+        case 8:
+            mes = "agosto";
+            break;
+        case 9:
+            mes = "setmbro";
+            break;
+        case 10:
+            mes = "outubro";
+            break;
+        case 11:
+            mes = "novembro";
+            break;
+        case 12:
+            mes = "dezembro";
+            break;
+        default:
+            mes = "Mês inválido!";
+            break;
+    }
+    return mes;
+}
+
+string MesExtenso2(int mesNumero)
+{
+    string[] meses = { "jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez" };
+
+    string mes = String.Empty; // = ""
+    if (mesNumero < 1 || mesNumero > 12)
+    {
+        mes = "Mês inválido!";
+    }
+    else
+    {
+        mes = meses[mesNumero - 1];
+    }
+    return mes;
+}
+
+
+void FormataData(int dd, int mm, int aaaa)
+{
+    string mes = MesExtenso(mm);
+    // exemplo de saída: 29 de março de 2022
+    Console.WriteLine($"{dd} de {mes} de {aaaa}");
+}
+
+
+// Main
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+
+FormataData(29, 3, 2022);
+
+Console.Write("Dia........: ");
+int dia = int.Parse(Console.ReadLine());
+
+Console.Write("Mês Numero.: ");
+int mes = int.Parse(Console.ReadLine());
+
+Console.Write("Ano........: ");
+int ano = int.Parse(Console.ReadLine());
+
+FormataData(dia, mes, ano);
