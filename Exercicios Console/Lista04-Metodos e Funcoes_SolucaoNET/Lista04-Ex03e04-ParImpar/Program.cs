3e53886e0d765f7445814426836c03bfa67f0389
jjmoreira-projects <59443855+jjmoreira-projects@users.noreply.github.com>
2022-06-05 00:21:30 +0100

    Add files via upload

---
+++ Exercicios Console/Lista04-Metodos e Funcoes_SolucaoNET/Lista04-Ex03e04-ParImpar/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+// 3.Escreva uma FUNÇÃO que receba um número e
+// retorne -1 se o número for ímpar ou 1 se for par.
+int VerificaParImpar(int numero)
+{
+    if (numero % 2 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+// 4. Escreva um PROCEDIMENTO que receba um número e
+// escreve se o número é ímpar ou se é par.
+void EscreveParImpar(int numero)
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("PAR");
+    }
+    else
+    {
+        Console.WriteLine("IMPAR"); 
+    }
+}
+
+// Main
+// invocar a função (método) para o numero 10 e 5 
+// para ter acesso ao retorno da função, criamos uma variavel
+// ou usamos o resultado diretamente (por exemplo no Console.WriteLine())
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.WriteLine("FUNÇÃO, Numero 10 e 5");
+int resultado = VerificaParImpar(10);
+Console.WriteLine($"Resultado: {resultado}");
+resultado = VerificaParImpar(5);
+Console.WriteLine($"Resultado: {resultado}");
+
+// Procedimento
+Console.WriteLine();
+Console.WriteLine("PROCEDIMENTO, Numero 10 e 5");
+EscreveParImpar(10);
+EscreveParImpar(5);
+
+// Pedir o numero ao utilizador
+Console.Write("Numero para analisar: ");
+int num = int.Parse(Console.ReadLine());
+resultado = VerificaParImpar(num);
+Console.WriteLine($"Resultado: {resultado}");
+
---
+++ Exercicios Console/Lista04-Metodos e Funcoes_SolucaoNET/Lista04-Ex05-Pontos/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Escreva uma função que receba como parâmetro
+// o número de vitórias e o número de empates de uma equipa de futebol,
+// calcule e retorne os pontos tendo em conta que:
+// uma vitoria são 3 pontos e um empate é 1 ponto.
+
+
+int CalculaPontos(int vitorias, int empates)
+{
+    int pontos;
+    pontos = vitorias * 3 + empates;
+    return pontos;
+}
+
+int CalculaPontos3(int vitorias, int empates)
+{  
+    return vitorias * 3 + empates;
+}
+
+void CalculaPontos2()
+{
+
+}
+
+// Main
+// invocar a função (método) para 5 vitorias e 3 empates 
+// para ter acesso ao retorno da função, criamos uma variavel
+// ou usamos o resultado diretamente (por exemplo no Console.WriteLine())
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+
+int resultado =  CalculaPontos(5, 3);
+Console.WriteLine($"Pontos: {resultado}");
+// ou 
+Console.WriteLine($"Pontos: {CalculaPontos(5,3)}");
+
+// Pedir as vitorias e empates ao utilizador
+Console.Write("Quantidade de vitorias: ");
+int vit = int.Parse(Console.ReadLine());
+
+Console.Write("Quantidade de empates: ");
+int emp = int.Parse(Console.ReadLine());
+
+resultado = CalculaPontos(vit, emp);
+
+Console.WriteLine($"Um clube com {vit} vitórias e {emp} empates tem {resultado} pontos");
