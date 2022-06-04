3db28a40d7b748fdf8b02d1a334fddada1abd6fd
jjmoreira-projects <59443855+jjmoreira-projects@users.noreply.github.com>
2022-06-05 00:03:23 +0100

    Add files via upload

---
+++ Exercicios WinForms/02-EstudoEstruturasComArrays/EstudoEstruturasComArrays/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace EstudoEstruturasComArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxDados = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNumero = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxNota = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxDados
+            // 
+            this.listBoxDados.FormattingEnabled = true;
+            this.listBoxDados.ItemHeight = 15;
+            this.listBoxDados.Location = new System.Drawing.Point(319, 56);
+            this.listBoxDados.Name = "listBoxDados";
+            this.listBoxDados.Size = new System.Drawing.Size(282, 169);
+            this.listBoxDados.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dados do arrayRegistos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nota:";
+            // 
+            // textBoxNumero
+            // 
+            this.textBoxNumero.Location = new System.Drawing.Point(114, 79);
+            this.textBoxNumero.Name = "textBoxNumero";
+            this.textBoxNumero.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNumero.TabIndex = 3;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(114, 117);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(180, 23);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // textBoxNota
+            // 
+            this.textBoxNota.Location = new System.Drawing.Point(114, 154);
+            this.textBoxNota.Name = "textBoxNota";
+            this.textBoxNota.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNota.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Dados";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(114, 201);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(180, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Adicionar ao arrayRegistos";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 344);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxNota);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxNumero);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxDados);
+            this.Name = "Form1";
+            this.Text = "Estrutras com Arrays";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxDados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNumero;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxNota;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ Exercicios WinForms/02-EstudoEstruturasComArrays/EstudoEstruturasComArrays/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstudoEstruturasComArrays
+{
+    public partial class Form1 : Form
+    {
+
+        // definir estrutura de camopos
+        struct Registo
+        {
+            public int numero;
+            public string nome;
+            public double nota;
+        }
+
+
+        // definir uma array do tipo da estrutura
+        Registo[] arrayRegistos = new Registo[10];
+
+        int numeroRegistos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // preencher o 1ª celula do array 
+            arrayRegistos[0].numero = 100;
+            arrayRegistos[0].nome = "ISLA";
+            arrayRegistos[0].nota = 20;
+            numeroRegistos = 1;
+
+            MostraNaLista();
+        }
+
+      
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+            bool sucessoNumero = int.TryParse(textBoxNumero.Text, out int numero);
+            bool sucessoNota = double.TryParse(textBoxNumero.Text, out double nota);
+
+            if (sucessoNumero == false || sucessoNota == false)
+            {
+                MessageBox.Show("Dados invalidos!", "Estruturas com arrays", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (numeroRegistos == 9)
+            {
+                MessageBox.Show("Limite do array alcançado!", "Estruturas com arrays", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int indice = numeroRegistos + 1;
+
+            arrayRegistos[indice].numero = numero;
+            arrayRegistos[indice].nome = textBoxNome.Text;
+            arrayRegistos[indice].nota = nota;
+
+            numeroRegistos++;
+
+            textBoxNumero.Clear();
+            textBoxNome.Clear();
+            textBoxNota.Clear();
+            textBoxNumero.Focus();
+
+            MostraNaLista();
+
+        }
+        void MostraNaLista()
+        {
+            listBoxDados.Items.Clear();
+
+            for (int i = 0; i < arrayRegistos.Length; i++)
+            {
+                if (arrayRegistos[i].numero != 0)
+                {
+                    listBoxDados.Items.Add(arrayRegistos[i].numero +
+                                           "-" + arrayRegistos[i].nome +
+                                           "-" + arrayRegistos[i].nota);
+                }
+            }
+
+        }
+
+    }
+}
